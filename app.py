8e1b322ef5d3c5f8180dc9affed937154df54090
George Olson <golson@pdx.edu>
2024-11-12 11:02:12 -0800

    Updated port to run docker container

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ app.add_url_rule('/spotify_results',
                  methods=["GET"])
                  
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
